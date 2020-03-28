ade06491507eecd7890d6fbfcaadc9c4f7291a7f
Marc Sabates Gelpi <marc.sabates.gelpi@protonmail.com>
2020-03-28 18:44:19 +0000

    Add Read hex or binary

--- src/cipher_sabre/core.clj
+++ src/cipher_sabre/core.clj
@@ -1,7 +1,8 @@
 (ns cipher-sabre.core
   (:require [byte-streams :as streams]
             [cipher-sabre.cipher :as cipher]
-            [clojure.java.io :refer [file output-stream]]))
+            [clojure.java.io :refer [file output-stream]]
+            [clojure.edn :as edn]))
 
 (defn write-binary-file
   [s v]
@@ -9,38 +10,71 @@
     (.write out (byte-array (count v) v))))
 
 (defn read-binary-file
-  [s]
-  (streams/to-byte-array (file s)))
+  [path]
+  (streams/to-byte-array (file path)))
+
+(defn- throw-if
+  "Throw `Exception` when (pred x) is `true`; otherwise return `x`."
+  [pred x]
+  (if (pred x)
+    (throw (Exception. (format "Exception for %s" x)))
+    x))
+
+(defn- bin-hex?
+  [x]
+  (or (<= 48 x 57) ;; 0-9
+      (<= 97 x 102) ;; a-f
+      (<= 65 x 70) ;; A-F
+      ))
+
+(defn read-hex-file
+  "Process an hex file until it reaches the end; throw `Exception` if no hex char found."
+  [path]
+  (sequence (comp (remove #{\newline})
+                  (map (partial throw-if (complement (comp bin-hex? int))))
+                  (partition-all 2)
+                  (map (partial apply str "0x"))
+                  (map edn/read-string))
+            (slurp path)))
+
+(defn read-ciphered-file
+  [path]
+  (try (read-hex-file path)
+       (catch Exception _ (read-binary-file path))))
+
+(defn- text-file->ints
+  [path]
+  (map int (slurp path)))
 
 (defn cipher-command
-  ([key file]
-   (cipher-command key file nil))
-  ([key file cycles]
+  ([key path]
+   (cipher-command key path nil))
+  ([key path cycles]
    (let [iv (cipher/random 10)]
-     (->> (cipher/init-and-cipher (map int (slurp file)) key iv cycles)
+     (->> (cipher/init-and-cipher (text-file->ints path) key iv cycles)
           (concat (map int iv))
           (write-binary-file "ciphered.out")))))
 
 (defn decipher-command
-  ([key file]
-   (decipher-command key file nil))
-  ([key file cycles]
-   (let [all (map int (read-binary-file file))
+  ([key path]
+   (decipher-command key path nil))
+  ([key path cycles]
+   (let [all (map int (read-ciphered-file path))
          iv (take 10 all)
          content (drop 10 all)]
      (->> (cipher/init-and-cipher content key iv cycles)
           (write-binary-file "clear-text.out")))))
 
 (defn run-command
-  [command key file cycles]
+  [command key path cycles]
   (case command
-    "cipher" (cipher-command key file cycles)
-    "decipher" (decipher-command key file cycles)
+    "cipher" (cipher-command key path cycles)
+    "decipher" (decipher-command key path cycles)
     nil))
 
 (defn -main
   "Entry point for CipherSabre."
-  [command key file & [cycles]]
-  (if (and (seq key) (seq file))
-    (run-command command key file cycles)
-    (println "key and file are mandatory.. Try again with clj -m cipher-sabre.core \"key\" \"file\"")))
+  [command key path & [cycles]]
+  (if (and (seq key) (seq path))
+    (run-command command key path cycles)
+    (println "key and file are mandatory.. Try again with clj -Arun \"key\" \"file\"")))
